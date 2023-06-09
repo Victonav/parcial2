c06dda975f9c84aa4b0d152a484cfdba003f181b
Victoria <victoria.cnavw@gmail.com>
2023-06-09 19:36:14 -0300

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Taylor Swift, Fearless, Speak Now, Red, 1989, Rep, Folklore, Evermore, Midnights"
+
+if __name__ == "__main__": 
+    app.run()
+
