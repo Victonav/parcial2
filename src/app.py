2926e789486651f6e5f2c90658d3e669cbfd09dd
Victoria <victoria.cnavw@gmail.com>
2023-06-09 20:58:28 -0300

    No funciona

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app= Flask(__name__)
 @app.route("/")
 def index():
     
-    return "Taylor Swift, Fearless, Speak Now, Red, 1989, Rep, Folklore, Evermore, Midnights"
+    return "Taylor Swift, Fearless, Speak Now(TV), Red, 1989, Rep, Folklore, Evermore, Midnights"
 
 if __name__ == "__main__": 
     app.run()
