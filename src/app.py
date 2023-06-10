c512e03dd822e337918cbb4eb414f0458a7b8c7b
Victoria <victoria.cnavw@gmail.com>
2023-06-09 21:12:11 -0300

    otra

--- src/app.py
+++ src/app.py
@@ -4,7 +4,6 @@ app= Flask(__name__)
 
 @app.route("/")
 def index():
-    x=1
     return "Taylor Swift, Fearless, Speak Now, Red, 1989, Rep, Folklore, Evermore, Midnights"
 
 if __name__ == "__main__": 
